a9c006dfd20f577416f62c2a3c32a3aeb4682014
linnamuri <lakshmi.innamuri@gmail.com>
2022-08-19 22:10:33 -0400

    Finding Lists

--- src/Mars.java
+++ src/Mars.java
@@ -14,9 +14,6 @@ public class Mars {
 //Create a boolean variable named Landing, and set it equal to true
         boolean landing = true;
 
-//The landing process takes 2 days, calculate how much food is left after landing
-//Equation: Food = Food - (Pop * 0.75);
-//Use the equation twice
         shipFood = shipFood - (shipPopulation * 0.75);
         System.out.println("The amount of food that is left after consuming for 1 day : " + shipFood);
 
@@ -30,19 +27,17 @@ public class Mars {
         shipPopulation += 5;
         System.out.println("Ship population after joining new babies: " + shipPopulation);
 
-        //Choose where you want the ship to land. The Hill, The Plain, or The Ocean
-//Create a String value called LandingLocation and set it equal to "The Hill" or "The Plain" or "The Ocean"
         String landingLocation = "The Hill";
-//Create an if else statement. That checks to see if the LandingLocation is equal to "The Plain" ignoring case
         if (landingLocation.equals("thePlain")) {
             System.out.println("Bbzzz Landing on the Plain");
         } else {
             System.out.println("ERROR!!! Flight plan already set. Landing on the Plain");
         }
         try {
-            //landing = landingCheck(1);
-            //GuessingGame gm=new GuessingGame();
-            MarsExpedition me=new MarsExpedition();
+            landing = landingCheck(1);
+            GuessingGame gm=new GuessingGame();
+            MarsExpedition me =new MarsExpedition();
+            FindingsLists fl =new FindingsLists();
 
         }
         catch (Exception e)
